7fda015b443e90ea7ea6ef136ff9116adc3cb127
Marat Gumarov <mgumarov@provectus.com>
2022-05-31 15:49:34 +0300

    trigger wf

--- kafka-ui-api/src/main/java/com/provectus/kafka/ui/KafkaUiApplication.java
+++ kafka-ui-api/src/main/java/com/provectus/kafka/ui/KafkaUiApplication.java
@@ -11,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class KafkaUiApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(KafkaUiApplication.class, args);
+    SpringApplication
+        .run(KafkaUiApplication.class, args);
   }
 }
